defb38983e239ac28629ea6537d6c6373f5eedf4
Zack <lockzy@hotmail.com>
2025-09-17 18:44:20 +0800

    Added html and js file and added computerchoice function that returns scissors papers or stone randomly

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Testing123")
+
+// Make a function that gets a 
+function computerChoice() {
+    
+let number = Math.floor(Math.random() * 3) + 1;
+if (number === 1)
+    return "Scissors";
+else if (number === 2)
+    return "Rock";
+else (number === 3)
+    return "Paper"
+}
+
+console.log(computerChoice());
+// Tag a number to scissors, papers and stone
+
+// Make the computerChoice function 
+
+// Make the userChoice function
+
+// Match the user function against the computer and decide who wins
