69f308f9cf9d98c63d6f1a076567f7e16701d997
Zack <lockzy@hotmail.com>
2025-09-17 20:05:33 +0800

    Added game logic and 5 round logic and completed assignment

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-console.log("Testing123")
 
 // Make a function that gets a 
 function computerChoice() {
     
 let number = Math.floor(Math.random() * 3) + 1;
 if (number === 1)
-    return "Scissors";
+    return "scissors";
 else if (number === 2)
-    return "Rock";
+    return "rock";
 else (number === 3)
-    return "Paper"
+    return "paper";
 }
 
-console.log(computerChoice());
-// Tag a number to scissors, papers and stone
+// Initialize scores
+let humanScore = 0;
+let computerScore = 0;
 
-// Make the computerChoice function 
+// Play round function
+// 
+function playround() {
+    const cpuChoice = computerChoice();
+    const choice = prompt("Scissors, Papers or Rock?");
+    
+    const rules = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper",
+    }
+    console.log(`Computer chose: ${cpuChoice}`);
+    console.log(`You chose: ${choice}`);
 
-// Make the userChoice function
+    if (choice === cpuChoice){
+        console.log("Its a tie")
+    }
+    else if (rules[choice] === cpuChoice){
+        console.log("You Win this round!")
+        humanScore ++;
+        console.log(`Your Score is ${humanScore}!`);
+        console.log(`Computer Score is ${computerScore}!`);
+    }
+    else {
+        console.log("You lose this round!");
+        computerScore ++;
+        console.log(`Your Score is ${humanScore}!`);
+        console.log(`Computer Score is ${computerScore}!`);
+    }
+} 
 
-// Match the user function against the computer and decide who wins
+//Start game 5 rounds and print who wins
+function startGame() {
+for (let i = 0; i < 5; i++) {
+    
+        playround()
+        
+    }
+    if (humanScore > computerScore ) {
+        console.log(`You win!`)
+    }
+    else {
+        console.log(`CPU win!`)
+    } 
+}
+
+startGame();
+
